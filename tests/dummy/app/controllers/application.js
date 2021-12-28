c59df9625cc437e3a8cac3611c09f3364c9f6d1f
Oleg Larkin <oleg.larkin@nordigy.ru>
2021-12-28 19:07:53 +0300

    chore: tests added

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,12 +1,16 @@
 import Controller from '@ember/controller';
-import { DIRECTION_ALL, DIRECTION_HORIZONTAL, DIRECTION_VERTICAL } from 'draggable-modifier';
+import {
+  DIRECTION_ALL,
+  DIRECTION_HORIZONTAL,
+  DIRECTION_VERTICAL,
+} from 'draggable-modifier';
 
 export default class ApplicationController extends Controller {
-    DIRECTION_ALL = DIRECTION_ALL;
-    DIRECTION_HORIZONTAL = DIRECTION_HORIZONTAL;
-    DIRECTION_VERTICAL = DIRECTION_VERTICAL;
+  DIRECTION_ALL = DIRECTION_ALL;
+  DIRECTION_HORIZONTAL = DIRECTION_HORIZONTAL;
+  DIRECTION_VERTICAL = DIRECTION_VERTICAL;
 
-    handlePanMove(ev, cb) {
-        return cb(ev);
-    }
+  handlePanMove(ev, cb) {
+    return cb(ev);
+  }
 }
